c737c6005ef7b0c8e7cb6c440647cc2af7d719a9
Arup Debnath <1805043@ugrad.cse.buet.ac.bd>
2023-05-03 18:11:24 +0600

    Added api endpoints for chefs

--- index.js
+++ index.js
@@ -2,9 +2,19 @@ const express = require('express')
 const app = express()
 const cors = require('cors')
 
+const chefs = require('./data/chefs.json');
+
 app.use(express.json())
 app.use(cors())
 
+app.get('/chefs', (req, res) => {
+    res.send(chefs);
+})
+
+app.get('/chefs/:id', (req, res) => {
+    const chef = chefs.find(chef => chef.id === req.params.id)
+    res.send(chef);
+})
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
